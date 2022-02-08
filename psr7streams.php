c233826606aa3489103a6a62fe863c755aeb7959
Jeffrey Tocco <jnt369@gmail.com>
2022-02-08 17:28:15 -0500

    steams and bodies

---
+++ psr7streams.php
@@ -0,0 +1,18 @@
+<?php 
+require 'vendor/autoload.php';
+use GuzzleHttp\Psr7;
+
+$stream = Psr7\Utils::streamFor('Sample string');
+
+echo $stream . "\r\n"; 
+echo $stream->getSize() . "\r\n";
+echo $stream->isReadable() . "\r\n";
+echo $stream->isWritable() . "\r\n";
+echo $stream->isSeekable() . "\r\n";
+
+$stream->write('test');
+$stream->rewind();
+echo $stream->read(5) . "\r\n";
+echo $stream->getContents() . "\r\n";
+echo $stream->eof() . "\r\n";
+?>
